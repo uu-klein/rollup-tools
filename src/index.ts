767022632737f78d04821f87174da6a03d15fe7d
tyson <obgztyson@gmail.com>
2021-04-02 17:25:33 +0800

    整合

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /*
  * @Author: tyson
- * @LastEditTime: 2021-04-01 21:11:35
+ * @LastEditTime: 2021-04-02 17:25:06
  * @LastEditors: Please set LastEditors
  * @Description: 入口文件
  * @FilePath: \obtool\src\index.ts
@@ -11,7 +11,5 @@
 import '../css/base.css';
 
 import '../css/normalize.css';
-
-import '../css/test.css';
 
 export * from '../util';
